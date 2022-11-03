02d154cca7f31ec6e39bbb8648decee530dd7d07
Victor Pagnozi <victor.pagnozi@gmail.com>
2022-11-03 19:30:20 -0300

    test

--- agropedia-web/src/App.js
+++ agropedia-web/src/App.js
@@ -6,6 +6,11 @@ function App() {
     <div className="App">
       <h1>Test</h1>
       <h2>test</h2>
+
+      <div className="test">
+        <h4>ashuda</h4>
+        <span>lll</span>
+      </div>
     </div>
   );
 }
